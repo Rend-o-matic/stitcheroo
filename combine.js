4b9ee43267564c4f9579a9c5bdfe2fcf0a88dd64
Sean Tracey <seanmtracey@gmail.com>
2020-05-28 00:43:24 +0100

    Single line output.
    Additional deps

--- combine.js
+++ combine.js
@@ -24,25 +24,32 @@ ffmpeg -loop 1 -i vid_back.png -i 1.mov -i 2.mov -i 3.mov -filter_complex \
  -map "[olOZJoXQdm9V]" -movflags +faststart  -filter_complex amerge=inputs=3 -ac 3 output.mp4 -y
 */
 
+const ffprobe = require('ffprobe-static');
+const ffmpeg_path = require('ffprobe-static').path;
 const shortid = require('shortid').generate;
+const boxjam = require('boxjam');
 
 const inputFiles = ['1.mov', '2.mov', '3.mov'];
-const boxes = [{"id":1,"width":934,"height":526,"x":21,"y":9},{"id":2,"width":934,"height":526,"x":965,"y":9},{"id":3,"width":934,"height":526,"x":493,"y":545}];
+const boxes = [ 
+    {"id" : 1, "width" : 934, "height" : 526, "x" : 21, "y" : 9},
+    {"id" : 2, "width" : 934, "height" : 526, "x" : 965, "y" : 9},
+    {"id" : 3, "width" : 934, "height" : 526, "x" : 493, "y" : 545}
+];
 
 const BASE_CMD = `ffmpeg -loop 1 -i vid_back.png`;
 
 const CMD_WITH_INPUTS = `${BASE_CMD} -i ${inputFiles.join(' -i ')}`;
 
-let FILTER = `"[0:v]scale=1920:-1[bg];\\\n`
+let FILTER = `"[0:v]scale=1920:-1[bg]; `
 
 // Scale inputs
 for(let i = 0; i < inputFiles.length; i += 1){
 
-    FILTER += `[${i + 1}:v]scale=${boxes[i].width}:${boxes[i].height},setpts=PTS-STARTPTS[fg${i + 1}];\\\n`
+    FILTER += `[${i + 1}:v]scale=${boxes[i].width}:${boxes[i].height},setpts=PTS-STARTPTS[fg${i + 1}]; `
 
 }
 
-FILTER += `[bg][fg1]overlay=${boxes[0].x}:${boxes[0].y}:shortest=1[ol0];\\\n`
+FILTER += `[bg][fg1]overlay=${boxes[0].x}:${boxes[0].y}:shortest=1[ol0]; `
 
 // Overlay inputs
 
@@ -52,12 +59,13 @@ for(let j = 1; j < inputFiles.length; j += 1){
 
     const thisKey = `ol${shortid()}`;
 
-    FILTER += `[${lastKey}][fg${j + 1}]overlay=${boxes[j].x}:${boxes[j].y},format=yuv420p[${thisKey}]${ j === inputFiles.length -1 ? '"\\' : ';\\' }\n`
+    FILTER += `[${lastKey}][fg${j + 1}]overlay=${boxes[j].x}:${boxes[j].y},format=yuv420p[${thisKey}]${ j === inputFiles.length -1 ? '"' : ';' }`
     lastKey =  thisKey;
 
 }
 
 const OUTPUT_FLAGS = `-map "[${lastKey}]" -movflags +faststart  -filter_complex amerge=inputs=${inputFiles.length} -ac ${inputFiles.length} output.mp4 -y`
 
+const FINAL_CMD = `${CMD_WITH_INPUTS} -filter_complex   ${FILTER} ${OUTPUT_FLAGS}`;
 
-console.log(CMD_WITH_INPUTS + ' -filter_complex \\\n' + FILTER + ` ${OUTPUT_FLAGS}`);
+console.log(FINAL_CMD);
