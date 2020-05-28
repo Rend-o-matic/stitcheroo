f10e5ba8d1b81f4baea3820425e23083bb778ea4
Sean Tracey <seanmtracey@gmail.com>
2020-05-28 01:31:45 +0100

    Probe input videos to generate dynamic output

--- combine.js
+++ combine.js
@@ -1,71 +1,84 @@
-// Manually input
-/*
-ffmpeg -loop 1 -i vid_back.png -i 1.mov -i 2.mov -i 3.mov -filter_complex \
-"[0:v]scale=1920:-1[bg]; \
- [1:v]scale=934:526,setpts=PTS-STARTPTS[fg1]; \
- [2:v]scale=934:526,setpts=PTS-STARTPTS[fg2]; \
- [3:v]scale=934:526,setpts=PTS-STARTPTS[fg3]; \
- [bg][fg1]overlay=21:9:shortest=1[ol1]; \
- [ol1][fg2]overlay=965:9,format=yuv420p[om];\
- [om][fg3]overlay=493:545,format=yuv420p[v]" \
--map "[v]" -movflags +faststart  -filter_complex amerge=inputs=3 -ac 3 output.mp4 -y
-*/
-
-// Software Generated
-/*
-ffmpeg -loop 1 -i vid_back.png -i 1.mov -i 2.mov -i 3.mov -filter_complex \
-"[0:v]scale=1920:-1[bg];\
-[1:v]scale=934:526,setpts=PTS-STARTPTS[fg1];\
-[2:v]scale=934:526,setpts=PTS-STARTPTS[fg2];\
-[3:v]scale=934:526,setpts=PTS-STARTPTS[fg3];\
-[bg][fg1]overlay=21:9:shortest=1[ol0];\
-[ol0][fg2]overlay=965:9,format=yuv420p[ol0ys2Fi9_5];\
-[ol0ys2Fi9_5][fg3]overlay=493:545,format=yuv420p[olOZJoXQdm9V]"\
- -map "[olOZJoXQdm9V]" -movflags +faststart  -filter_complex amerge=inputs=3 -ac 3 output.mp4 -y
-*/
-
+const child_process = require('child_process');
 const ffprobe = require('ffprobe-static');
 const ffmpeg_path = require('ffprobe-static').path;
 const shortid = require('shortid').generate;
 const boxjam = require('boxjam');
 
-const inputFiles = ['1.mov', '2.mov', '3.mov'];
-const boxes = [ 
-    {"id" : 1, "width" : 934, "height" : 526, "x" : 21, "y" : 9},
-    {"id" : 2, "width" : 934, "height" : 526, "x" : 965, "y" : 9},
-    {"id" : 3, "width" : 934, "height" : 526, "x" : 493, "y" : 545}
-];
+const inputFiles = ['1.mov', '2_mod.mov', '3_mod.mov', '1.mov'].map(filePath => {
+    return new Promise( (resolve, reject) => {
+        child_process.exec(`${ffprobe.path} -v error -show_entries stream=width,height -of default=noprint_wrappers=1:nokey=1 ${filePath}`, (error, stdout, stderr) => {
+            if (error) {
+              console.error(`exec error: ${error}`);
+              reject();
+            }
 
-const BASE_CMD = `ffmpeg -loop 1 -i vid_back.png`;
+            const r = stdout.split('\n');
+            const data = {
+                width : Number(r[0]),
+                height : Number(r[1]),
+                path : filePath
+            };
 
-const CMD_WITH_INPUTS = `${BASE_CMD} -i ${inputFiles.join(' -i ')}`;
+            resolve(data);
 
-let FILTER = `"[0:v]scale=1920:-1[bg]; `
+        });
+    } )
+});
 
-// Scale inputs
-for(let i = 0; i < inputFiles.length; i += 1){
+Promise.all(inputFiles)
+    .then(results => {
+        console.log(results);
 
-    FILTER += `[${i + 1}:v]scale=${boxes[i].width}:${boxes[i].height},setpts=PTS-STARTPTS[fg${i + 1}]; `
+        const boxes = boxjam(results, {width : 1920, height : 1080}, 10, true);
 
-}
+        const BASE_CMD = `ffmpeg -loop 1 -i vid_back.png`;
 
-FILTER += `[bg][fg1]overlay=${boxes[0].x}:${boxes[0].y}:shortest=1[ol0]; `
+        const CMD_WITH_INPUTS = `${BASE_CMD} -i ${boxes.map(info => {return info.path}).join(' -i ')}`;
 
-// Overlay inputs
+        console.log(CMD_WITH_INPUTS);
 
-let lastKey = "ol0";
+        let FILTER = `"[0:v]scale=1920:-1[bg]; `
 
-for(let j = 1; j < inputFiles.length; j += 1){
+        // Scale inputs
+        for(let i = 0; i < inputFiles.length; i += 1){
 
-    const thisKey = `ol${shortid()}`;
+            FILTER += `[${i + 1}:v]scale=${boxes[i].width}:${boxes[i].height},setpts=PTS-STARTPTS[fg${i + 1}]; `
 
-    FILTER += `[${lastKey}][fg${j + 1}]overlay=${boxes[j].x}:${boxes[j].y},format=yuv420p[${thisKey}]${ j === inputFiles.length -1 ? '"' : ';' }`
-    lastKey =  thisKey;
+        }
 
-}
+        FILTER += `[bg][fg1]overlay=${boxes[0].x}:${boxes[0].y}:shortest=1[ol0]; `
 
-const OUTPUT_FLAGS = `-map "[${lastKey}]" -movflags +faststart  -filter_complex amerge=inputs=${inputFiles.length} -ac ${inputFiles.length} output.mp4 -y`
+        // Overlay inputs
 
-const FINAL_CMD = `${CMD_WITH_INPUTS} -filter_complex   ${FILTER} ${OUTPUT_FLAGS}`;
+        let lastKey = "ol0";
 
-console.log(FINAL_CMD);
+        for(let j = 1; j < inputFiles.length; j += 1){
+
+            const thisKey = `ol${shortid()}`;
+
+            FILTER += `[${lastKey}][fg${j + 1}]overlay=${boxes[j].x}:${boxes[j].y},format=yuv420p[${thisKey}]${ j === inputFiles.length -1 ? '"' : ';' }`
+            lastKey =  thisKey;
+
+        }
+
+        const OUTPUT_FLAGS = `-map "[${lastKey}]" -movflags +faststart  -filter_complex amerge=inputs=${inputFiles.length} -ac ${inputFiles.length} output_dynamic.mp4 -y`
+
+        const FINAL_CMD = `${CMD_WITH_INPUTS} -filter_complex   ${FILTER} ${OUTPUT_FLAGS}`;
+
+        console.log(FINAL_CMD);
+
+        child_process.exec(FINAL_CMD, (error, stdout, stderr) => {
+            if (error) {
+            console.error(`exec error: ${error}`);
+            return;
+            }
+            console.log(`stdout: ${stdout}`);
+            console.error(`stderr: ${stderr}`);
+        });
+
+
+    })
+    .catch(err => {
+        console.log('err:', err);
+    })
+;
